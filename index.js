af3c88a1a74cb25de319ebf1e267ceef470d275d
JDaniel <jensysantana@gmail.com>
2021-01-19 03:50:17 -0500

    new app commit1

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3300;
+
+// app.use('/', express.static(__dirname + 'html'));
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    // const uri = path.normalize(`${path.resolve()}/html/index.html`);
+    console.log('---------uri---------');
+    console.log(path.join(__dirname, 'public'));
+    console.log('---------uri---------');
+    //res.sendFile(path.join(uri));
+    return res.sendFile(path.join(__dirname, 'index'));
+
+
+    // res.status(200).json({
+    //     djksd: 'fdkjfdjbfklfd'
+    // })
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+
+
+module.exports = app;
