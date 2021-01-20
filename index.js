3451ffecf1bc9619c37cab80ced6820c6f1cff98
JDaniel <jensysantana@gmail.com>
2021-01-19 22:21:29 -0500

    image test8

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3300;
+const PORT = process.env.PORT || 4300;
 
 // app.use('/', express.static(__dirname + 'html'));
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     // const uri = path.normalize(`${path.resolve()}/html/index.html`);
@@ -13,7 +14,7 @@ app.get('/', (req, res) => {
     console.log(path.join(__dirname, 'public'));
     console.log('---------uri---------');
     //res.sendFile(path.join(uri));
-    return res.sendFile(path.join(__dirname, 'index'));
+    return res.render(path.join(__dirname, 'index'));
 
 
     // res.status(200).json({
