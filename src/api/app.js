d134892dc50f4fffffac836e3a23925274926223
JDaniel <jensysantana@gmail.com>
2021-01-20 02:13:42 -0500

    image try9

--- src/api/app.js
+++ src/api/app.js
@@ -3,10 +3,24 @@ const path = require('path');
 const app = express();
 const shopRT = require('./routes/routes');
 const webRoutes = require('../web/routes/routes');
+const morgan = require('morgan');
 
-// app.use('/', express.static(__dirname + 'html'));
+/* 
+ * Cors setup
+ */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, AUTH_TOKEN_X, AUTH_ROLE, auth_role_x, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept,  Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+// app.use(cors());
+// //enabled all cors
+app.use(express.urlencoded({ extended: false, limit: '100kb' }));
+app.use(express.json());
+app.use(morgan('dev'));
 app.use('/', express.static(path.join(__dirname, '../../public')));
-
 app.use('/api/v1/', shopRT);
 app.use('/', webRoutes);
 
@@ -15,8 +29,6 @@ console.log(path.join(__dirname));
 console.log(path.join(__dirname, '../../public'));
 console.log(path.join(__dirname, '../web/views'));
 console.log('---------uri-99999999--------');
-
-
 
 // app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, '../web/views'));
